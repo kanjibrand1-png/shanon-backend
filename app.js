0b4866cc8a559bcde043103085257a3bf2aaa3f1
alaeddine-cmd <alaeddine_12@yahoo.fr>
2025-05-15 17:59:38 +0100

    cors changes

--- app.js
+++ app.js
@@ -10,23 +10,12 @@ app.use(express.json());
 app.use('/api/demo', demoRoutes);
 app.use('/api/subscription', subscriptionRoutes);
 
-const allowedOrigins = [
-  'http://localhost:4028',
-  'https://shannon-vhdz.onrender.com'
-];
+app.use(cors());
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, or postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+  origin: ['https://shannon-vhdz.onrender.com/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Connect DB
