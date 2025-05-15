d310d828793ed419680ac52566e6c05d0a2757dc
alaeddine-cmd <alaeddine_12@yahoo.fr>
2025-05-15 19:03:42 +0100

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: ['https://shannon-vhdz.onrender.com/'],
+  origin: ['https://shannon-vhdz.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
