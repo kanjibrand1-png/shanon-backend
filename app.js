f8a4b687b54253532fe7f7bd55bfd6772f19d413
alaeddine-cmd <alaeddine_12@yahoo.fr>
2025-05-22 12:58:37 +0100

    changes

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ app.use(cors({
 
 app.use('/api/demo', demoRoutes);
 app.use('/api/subscription', subscriptionRoutes);
-app.post('/api/demo', (req, res) => {
-  console.log('Received request:', req.body);
-  res.status(200).json({ message: 'Demo request received' });
-});
+
 
 // Connect DB
 mongoose.connect(process.env.MONGO_URI, {
