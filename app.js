5b7829df00eb0425a26f4700b07c0275bd7ce45e
alaeddine-cmd <alaeddine_12@yahoo.fr>
2025-05-24 17:16:52 +0100

    changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT;
 // Start server
 connectDB()
   .then(() => {
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MONGO_URI);
     console.log("connected to database");
   } catch (err) {
     console.log("unable to connect to database", err.message);
